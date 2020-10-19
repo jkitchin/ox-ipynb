1f3ddf70dfdef22cf6063d5c93d69ab28012c3ff
John Kitchin <jkitchin@andrew.cmu.edu>
2020-10-19 15:34:31 -0400

    remove extra line to fix issue #35.
    I searched the git history, and I can't see where this was added. I also can't
    tell what it is for. I guess it would have made some kind of link to a keyword,
    but today that doesn't make sense to me.

--- ox-ipynb.el
+++ ox-ipynb.el
@@ -298,7 +298,6 @@ version was incorrectly modifying them."
   "Return the markdown cell for the string S."
   (let* ((org-export-filter-latex-fragment-functions '(ox-ipynb-filter-latex-fragment))
          (org-export-filter-link-functions '(ox-ipynb-filter-link))
-         (org-export-filter-keyword-functions '(ox-ipynb-keyword-link))
          ;; I overwrite the org function here because it does not give the right
          ;; levels otherwise. This one outputs exactly the level that is listed.
 	 ;; Also, I modify the table exporters here to get a markdown table for
